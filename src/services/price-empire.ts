d6917ae6e2cc03bbb04ef6b1d5de27a7406d8594
msgul <mertsamilgul@gmail.com>
2025-03-29 02:18:40 +0300

    add price-empire inventory v4

--- src/services/price-empire.ts
+++ src/services/price-empire.ts
@@ -9,6 +9,7 @@ class PriceEmpireService {
     private baseUrl = 'https://api.pricempire.com';
     private priceUrl = 'v3/items/prices';
     private inventoryUrl = 'v3/inventory';
+    private inventoryUrlV4 = 'v4/paid/inventory';
     private imageUrl = 'https://community.fastly.steamstatic.com/economy/image';
 
     async fetchPrices(appId: number) {
@@ -91,6 +92,65 @@ class PriceEmpireService {
 
             return inventory;
         } catch (error) {
+            throw new CustomError('Failed to fetch inventory', 500);
+        }
+    }
+
+    async fetchInventoryV4(steamId: string, appId: number) {
+        const url = `${this.baseUrl}/${this.inventoryUrlV4}?app_id=${appId}&steam_id=${steamId}&api_key=${this.apiKey}&force=true`;
+        try {
+            const response = await axios.get(url);
+            const items = response.data?.items;
+
+            const inventory = items
+                .filter((inv: any) => {
+                    return inv.item;
+                })
+                .map((inv: any) => {
+                    const item = inv.item;
+
+                    const splitName = item.market_hash_name.split(' | ');
+                    const specialName = item.market_hash_name.split(' - ')[1];
+                    const name =
+                        splitName.length > 1
+                            ? splitName[1].split(' (')[0] + (splitName[2] ? ' - ' + splitName[2] : '')
+                            : splitName[0].split(' (')[0];
+
+                    const image = item.image.includes('https://') ? item.image : `${this.imageUrl}/${item.image}`;
+                    const stickers = inv.stickers?.map((sticker: any) => {
+                        //if (!sticker?.image) return; // ??
+
+                        const image = sticker.image?.includes('https://')
+                            ? sticker.image
+                            : `${this.imageUrl}/${sticker.image}`;
+                        return {
+                            name: sticker.market_hash_name,
+                            image,
+                            wear: sticker.wear ? sticker.wear : undefined,
+                        };
+                    });
+
+                    return {
+                        assetId: inv.assetId,
+                        appId,
+                        tradable: !item.tradeLock, // not available in v4?
+                        family: splitName.length > 1 ? splitName[0] : undefined,
+                        name: specialName ? `${name} - ${specialName}` : name,
+                        marketName: item.market_hash_name,
+                        image,
+                        type: item.category,
+                        weapon: item.weapon_name,
+                        exterior: item.exterior ? item.exterior : undefined,
+                        stickers,
+                        lendable: lendableItemTypes.includes(item.category) && !item.tradeLock,
+                        price: item.price ? item.price : undefined,
+                        float: inv.float_value ? Number(Number(inv.float_value).toFixed(4)) : undefined,
+                    };
+                });
+
+            return inventory;
+        } catch (error) {
+            console.log(error);
             throw new CustomError('Failed to fetch inventory', 500);
         }
     }
